0ebe776263137636987ed194987445a4b8ed2e80
João Teixeira <joao@ycloud.com.br>
2021-03-28 21:36:34 -0300

    adds incement and decrement buttons

--- scoreboard-0.0.1.py
+++ scoreboard-0.0.1.py
@@ -20,9 +20,7 @@ clock = pygame.time.Clock()
 crashed = False
 playing = False
 
-timer_sec = 120
-timer_d_min = 10
-timer_d_sec = 0
+timer_sec = 0
 
 font = pygame.font.Font("alarm_clock.ttf", 200)
 font2 = pygame.font.Font(None, 80)
@@ -65,15 +63,14 @@ pause_btn_text = font3.render("Parar", True, white)
 timer = pygame.USEREVENT + 1                                                
 pygame.time.set_timer(timer, 1000)    # sets timer with USEREVENT and delay in milliseconds
 
-gameDisplay.fill(black)
-
-gameDisplay.blit(mm, mm_rect)
-gameDisplay.blit(p, p_rect)
-gameDisplay.blit(s1, s1_rect)
-gameDisplay.blit(s2, s2_rect)
-gameDisplay.blit(e1, e1_rect)
-gameDisplay.blit(e2, e2_rect)
-pygame.display.update()
+def handle_add_time(seconds):
+    print(seconds)
+    global timer_sec, mm
+    timer_sec += seconds
+    formatted = str(datetime.timedelta(seconds=timer_sec))[2:7]
+    mm = font.render(formatted, True, red)
+    timer = pygame.USEREVENT + 1                                                
+    pygame.time.set_timer(timer, 1000)
 
 while not crashed:
     for event in pygame.event.get():
@@ -88,21 +85,39 @@ while not crashed:
                     mm = font.render(formatted, True, red)               
             else:
                 pygame.time.set_timer(timer, 0)    # turns off timer event
+                playing = False
         #checks if a mouse is clicked
         if event.type == pygame.MOUSEBUTTONDOWN:
             #if the mouse is clicked on the
             # button the game is terminated
-            if 670 <= mouse[0] <= 670+140 and 120 <= mouse[1] <= 120+40:
+            if 670 <= mouse[0] <= 670+140 and 120 <= mouse[1] <= 120+40 and timer_sec > 0:
                 if playing == False:
                     playing = True
                 else:
                     playing = False
+            #if the mouse is clicked on the -1 button
+            if 52 <= mouse[0] <= 52+40 and 63 <= mouse[1] <= 63+50 and timer_sec > 60 and playing == False:
+                handle_add_time(-60)
+            #if the mouse is clicked on the +1 button
+            if 52 <= mouse[0] <= 52+40 and 113 <= mouse[1] <= 113+50 and playing == False:
+                handle_add_time(60)
+            #if the mouse is clicked on the +1 button
+            if 52 <= mouse[0] <= 52+40 and 163 <= mouse[1] <= 163+50 and playing == False:
+                handle_add_time(300)
 
     gameDisplay.fill(black)
     # stores the (x,y) coordinates into
     # the variable as a tuple
     mouse = pygame.mouse.get_pos()
 
+    pygame.draw.rect(gameDisplay,color_dark,[52,63,50,40])
+    gameDisplay.blit(font3.render("-1", True, white) , (65,70))
+    pygame.draw.rect(gameDisplay,color_dark,[52,113,50,40])
+    gameDisplay.blit(font3.render("+1", True, white) , (60,120))
+    pygame.draw.rect(gameDisplay,color_dark,[52,163,50,40])
+    gameDisplay.blit(font3.render("+5", True, white) , (60,170))
+
+    # Play / Pause button
     pygame.draw.rect(gameDisplay,color_dark,[670,120,100,40])
     # superimposing the text onto our button
     if playing:
